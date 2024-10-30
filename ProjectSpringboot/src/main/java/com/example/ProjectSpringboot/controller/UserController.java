eb0d1c7321060eef15169568d57560f1a21ff82d
= <=>
2024-10-30 18:10:53 +0700

    CRUD User

--- ProjectSpringboot/src/main/java/com/example/ProjectSpringboot/controller/UserController.java
+++ ProjectSpringboot/src/main/java/com/example/ProjectSpringboot/controller/UserController.java
@@ -3,9 +3,9 @@ package com.example.ProjectSpringboot.controller;
 
 import com.example.ProjectSpringboot.domain.User;
 import com.example.ProjectSpringboot.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,11 +16,31 @@ public class UserController {
 
     @PostMapping("/user/create")
     public User createUser(@RequestBody User postUser){
-
        User newUser =  this.userService.handleCreateUser(postUser);
-
        return newUser;
     }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id){
+        this.userService.handleDeleteUser(id);
+        return "bao";
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") long id){
+        return this.userService.fetchUserById(id);
+    }
+    @GetMapping("/user")
+    public List<User> getAllUser(){
+        return this.userService.fetchAllUser();
+    }
+    @PutMapping("user")
+    public User updateUser(@RequestBody User user){
+        User putUser = this.userService.handleUpdateUser(user);
+        return putUser;
+    }
+
+
 }
 
 
--- ProjectSpringboot/src/main/java/com/example/ProjectSpringboot/service/UserService.java
+++ ProjectSpringboot/src/main/java/com/example/ProjectSpringboot/service/UserService.java
@@ -5,13 +5,48 @@ import com.example.ProjectSpringboot.domain.User;
 import com.example.ProjectSpringboot.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
-    private final UserRepository UserRepository;
+
+    private final UserRepository userRepository;
     public UserService(UserRepository userRepository){
-        this.UserRepository = userRepository;
+        this.userRepository = userRepository;
     }
     public User handleCreateUser(User user){
-        return this.UserRepository.save(user);
+        return this.userRepository.save(user);
+    }
+
+    public void handleDeleteUser(long id){
+        this.userRepository.deleteById(id);
+    }
+
+    public User fetchUserById(long id){
+        // optional là có hay không
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if(userOptional.isPresent()){  // isPresent() có hay không
+            return userOptional.get();
+        }
+        return null;
+    }
+
+    public List<User> fetchAllUser(){
+        return this.userRepository.findAll();
+    }
+
+    /**
+     * gạch chân giá trị có thể null nếu cập nhập mà giá trị không thay đôi
+     */
+    public User handleUpdateUser(User reqUser){
+        User currentUser = this.fetchUserById(reqUser.getId());
+        if(currentUser != null){
+            currentUser.setEmail(reqUser.getEmail());
+            currentUser.setName(reqUser.getName());
+            currentUser.setPassword(reqUser.getPassword());
+            currentUser = this.userRepository.save(currentUser);
+        }
+        return currentUser;
     }
 }
